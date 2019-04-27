773a4dca8604929bb10e5d6f3fda5ff335b75708
MiguelRenda <miguel.renda@tecnico.ulisboa.pt>
2019-04-27 20:13:49 +0100

    fixes

--- Projeto.m
+++ Projeto.m
@@ -21,8 +21,7 @@ clear all, close all
 threshold = 125;
 minArea = 50;
 
-imgOriginal = imread('MATERIAL/database/Moedas3.jpg');
-%figure, imshow(imgOriginal); title('Original');
+imgOriginal = imread('MATERIAL/database/Moedas4.jpg');
 
 % PREPROCESSING
 
@@ -38,41 +37,6 @@ BW = imerode(BW,strel('disk',4));
 BW = imopen(BW,strel('disk',11));
 imgProcessed = BW;
 
-% 1- Count number of objects in the scene
-%[lb num]= bwlabel(BW);
-%figure, imshow(imgOriginal); title(['1- Number of objects: ', num2str(num)]);
-
-% 1- Count number of objects in the scene
-% 2- Visualization centroid, perimeter and area 
-% 3 - Relative distance of the objects
-% 4 - Derivative over the boundaries
 figure; 
 Information(imgOriginal, imgProcessed);
 
-
-
-% figure,
-% subplot(2,3,5); imshow(histeq(lb,10000)); title('Labels');
-% subplot(2,3,1); imshow(mat2gray(lb)); title('Labels BW');
-% colors = colormap(jet(9));
-% subplot(2,3,2); imshow(ind2rgb(lb,colors)); title('Labels');
-% stats = regionprops(lb);
-% areas = [stats.Area];
-% [dummy indM] = max(areas);
-% imgBr = (lb == indM);
-% subplot(2,3,3); imshow(imgBr); title('Maior �rea');
-% subplot(2,3,4); imshow(img.*uint8(imgBr)); title('C�rebro');
-% 
-% areas = [];
-% for k=1 : num
-%     areas = [areas length(find(lb==k))]
-% end
-% [ val ind] = max(areas)
-% 
-% num; 
-
-%[labeledImage, numberOfObject] = bwlabel(BW3); regionProps =
-%regionprops(labeledImage, 'Area', 'Centroid', 'Perimeter', 'FilledImage');
-%inds = find([regionProps.Area]>minArea);
-
-% ind;
